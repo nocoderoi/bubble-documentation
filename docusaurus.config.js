4d3386c0eb83fa2bfe11dce1e54c1184a20dc54d
efi rand <info@nocoderoi.com>
2025-09-24 12:34:44 +0300

    Remove logo link to root path in Docusaurus configuration.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,6 @@ const config = {
         logo: {
           alt: 'Bubble Logo',
           src: 'https://cdn.prod.website-files.com/659d6dd87d35d454a8ffd858/65a2817373352e86a37446e6_logo.svg',
-          to: '/',
         },
         items: [
           {
